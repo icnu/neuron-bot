f2577f597c5b2b7d749278d4ab42d53ea9d5e77d
icnu <172075965+icnu@users.noreply.github.com>
2025-07-17 18:57:21 +0530

    add discord bot

--- src/discord-proxy/src/index.ts
+++ src/discord-proxy/src/index.ts
@@ -1,6 +1,24 @@
 import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify'
 import { Server, IncomingMessage, ServerResponse } from 'http'
 import fastifyCors from '@fastify/cors'
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../../.env' });
+
+const client = new Client({ intents: [] });
+
+client.on(Events.ClientReady, readyClient => {
+  console.log(`Logged in as ${readyClient.user.tag}!`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
 
 const server: FastifyInstance = Fastify({})
 
@@ -12,7 +30,8 @@ server.get('/ping', async (request, reply) => {
 
 const start = async () => {
   try {
-    await server.listen({ port: 3000 })
+    await server.listen({ port: 3000 });
+    await client.login(process.env.DISCORD_BOT_TOKEN!);
 
     const address = server.server.address()
     const port = typeof address === 'string' ? address : address?.port
