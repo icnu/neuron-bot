f2a2dbec85fa00f2fb1751874a0b4b5fa01d5244
icnu <172075965+icnu@users.noreply.github.com>
2025-07-17 09:39:13 +0530

    setutp

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -14,6 +14,7 @@ import {
 } from "@/components/ui/dialog"
 import { useEffect, useState } from "react";
 import { AuthClient } from "@dfinity/auth-client";
+import { registry_canister } from "../../declarations/registry-canister";
 
 export default function Component() {
   const isConnected = false;
--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -2,12 +2,28 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import environment from 'vite-plugin-environment';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../../.env' });
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    emptyOutDir: true,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+    },
+  },
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    environment("all", { prefix: "CANISTER_" }),
+    environment("all", { prefix: "DFX_" }),
   ],
   resolve: {
     alias: {
