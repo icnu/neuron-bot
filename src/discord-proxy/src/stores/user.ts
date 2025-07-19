57b01633b9e809d941c8917a2dc95ef85c6d6a92
icnu <172075965+icnu@users.noreply.github.com>
2025-07-19 22:52:39 +0530

    added user store

---
+++ src/discord-proxy/src/stores/user.ts
@@ -0,0 +1,35 @@
+import { Principal } from "@dfinity/principal";
+import { get_encrypted_maps } from "../encrypted_maps";
+
+const _mapOwner = Principal.anonymous();
+const _mapName = new TextEncoder().encode("user_data");
+
+export type UserData = {
+    private_key_data: string,
+    delegation_data: string,
+    principal: string
+};
+
+export class UserStore {
+    async store(user_data: UserData) {
+        await get_encrypted_maps().setValue(
+            _mapOwner,
+            _mapName,
+            new TextEncoder().encode(user_data.principal),
+            new TextEncoder().encode(JSON.stringify(user_data))
+        );
+    }
+
+    async users(): Promise<UserData[]> {
+        const values = await get_encrypted_maps().getValuesForMap(
+            _mapOwner,
+            _mapName
+        );
+
+        return values.map(v => {
+            const json = new TextDecoder().decode(v[1]);
+            const parsed = JSON.parse(json) as UserData;
+            return parsed;
+        });
+    }
+}
