60553805361c50dec1e6efca6c85f1795482846c
icnu <172075965+icnu@users.noreply.github.com>
2025-07-23 18:47:23 +0530

    add governance service implementation

--- src/discord-proxy/src/governance/service.ts
+++ src/discord-proxy/src/governance/service.ts
@@ -1,19 +1,45 @@
-import { createActor } from "./bindings";
-import { ProposalData } from "./bindings/governance.did";
+import { Principal } from "@dfinity/principal";
+import { HttpAgent, Identity } from "@dfinity/agent";
+import { SnsGovernanceCanister, SnsNeuron, SnsNeuronId, SnsProposalData, SnsProposalId } from "@dfinity/sns";
 
 export class GovernanceServiceClass {
-    async listLastProposal(canisterId: string): Promise<ProposalData> {
-        const actor = await createActor(canisterId);
-        const proposals = await actor.list_proposals({
-            include_reward_status: [],
-            before_proposal: [],
-            limit: 1,
-            include_status: [],
-            exclude_type: [],
-            include_topics: []
+    async listLastProposal(canisterId: string): Promise<SnsProposalData> {
+        const governance = await SnsGovernanceCanister.create({
+            canisterId: Principal.fromText(canisterId)
+        });
+        const proposals = await governance.listProposals({
+            limit: 1
         });
 
         return proposals.proposals[0];
+    }
+
+    async listNeurons(canisterId: string, principal: string): Promise<SnsNeuron[]> {
+        const governance = await SnsGovernanceCanister.create({
+            canisterId: Principal.fromText(canisterId)
+        });
+        const neurons = await governance.listNeurons({
+            principal: Principal.fromText(principal),
+            limit: 10,
+        });
+
+        return neurons;
+    }
+
+    async vote(canisterId: string, identity: Identity, neuronId: SnsNeuronId, proposalId: SnsProposalId, vote: number) {
+        const governance = await SnsGovernanceCanister.create({
+            canisterId: Principal.fromText(canisterId),
+            agent: await HttpAgent.create({ identity })
+        });
+
+        await governance.manageNeuron({
+            subaccount: neuronId.id,
+            command: [{
+                'RegisterVote': {
+                    vote, proposal: [proposalId]
+                }
+            }]
+        });
     }
 }
 
